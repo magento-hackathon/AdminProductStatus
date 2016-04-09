322fc29293a54b67bc5906c8c3c6d63d5d725b4a
Ben Robie <brobie@degdigital.com>
2016-04-09 16:04:47 -0700

    Adding the preference for the Edit block

---
+++ Block/Catalog/Adminhtml/Product/Edit.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+/**
+ * Customer edit block
+ *
+ * @author      Magento Core Team <core@magentocommerce.com>
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ */
+namespace MagentoHackathon\AdminProductStatus\Block\Catalog\Adminhtml\Product;
+
+
+class Edit extends \Magento\Catalog\Block\Adminhtml\Product\Edit
+{
+    /**
+     * @var string
+     */
+    protected $_template = 'catalog/product/edit.phtml';
+
+}
